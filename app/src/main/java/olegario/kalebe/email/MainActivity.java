fb05799ac8bb13ce98771fdc77202956e9c0e6af
20221tiimi0190 <kalebe.olegario1850@gmail.com>
2024-04-16 09:16:38 -0300

    Atividade APP EMAIL

--- app/src/main/java/olegario/kalebe/email/MainActivity.java
+++ app/src/main/java/olegario/kalebe/email/MainActivity.java
@@ -1,6 +1,13 @@
 package olegario.kalebe.email;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +27,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        //Definicao da acao do click do botao
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Obtendo dados digitados pelo usuario
+                EditText etEmail = (EditText) findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto = (EditText) findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
+
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+                String[] emails = new  String[] {email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                try {
+                startActivity(Intent.createChooser(i, "Escolha o APP"));
+                }
+                catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Nao ha nenhum app que posso realizar essa operacao",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
